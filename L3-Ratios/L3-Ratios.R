7ce0f6ab4ddfcecb0a7302925c8d53a17cfcd73b
Landon Young <young518@uw.edu>
2017-01-25 10:20:15 -0800

    Work done on Lab 3

--- L3-Ratios/L3-Ratios.R
+++ L3-Ratios/L3-Ratios.R
@@ -1,7 +1,9 @@
 #Welcome to Lab 3, please answer the following questions
 
 #Install and require package MASS
-
+#install.packages("MASS")
+library(MASS)
+library(dplyr)
 
 #Using the built in dataset anorexia, aggregate the data to look at:
 #       1. the number of patients who received a treatment (choose one) who responded positively to treatment
@@ -9,18 +11,35 @@
 #       3. the number of patients who were in the control group who responded positively to treatment
 #       4. the number of patients who were in the control group who did not respond or responded negatively to treatment
 
+ano <- data.frame(anorexia)
+
+positive.cbt <- filter(ano, Treat == "CBT") %>% filter(Postwt > Prewt)
+nrow(positive.cbt)
+
+negative.cbt <- filter(ano, Treat == "CBT") %>% filter(Postwt < Prewt)
+nrow(negative.cbt)
+
+positive.cont <- filter(ano, Treat == "Cont") %>% filter(Postwt > Prewt)
+nrow(positive.cont)
+
+negative.cont <- filter(ano, Treat == "Cont") %>% filter(Postwt < Prewt)
+nrow(negative.cont)
+
+#summary <- group_by(ano, Treat) %>% 
+#           summarise(av.prewt = mean(Prewt), av.postwt = mean(Postwt), number = n())
+
 #Such that you end up with a 2 by 2 table
 
 
 #What kind of analysis appropriate to perform on a study like this?
-
-
-
+# Risk ratio
 
 #Perform the appropriate choice on the dataset
+cbt.ratio <- nrow(negative.cbt) / nrow(filter(ano, Treat == "CBT"))
+  
+cont.ratio <- nrow(negative.cont) / nrow(filter(ano, Treat == "Cont"))
 
-
-
+risk.ratio <- cbt.ratio/cont.ratio
 
 #What does your result tell you?
 
